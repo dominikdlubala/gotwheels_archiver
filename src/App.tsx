de737c491e4d31358174cc22c173e7670cc44722
Dominik Dłubała <ddlubala15@gmail.com>
2024-07-02 10:40:45 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';  
+import Root from './pages/Root'; 
+import HomePage from './pages/HomePage'; 
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <Root />, 
+    children: [
+      {
+        index: true, 
+        element: <HomePage />
+      }, 
+
+    ]
+  }
+])
+
+function App() {
+
+  return <RouterProvider router={router} />
+}
+
+export default App
